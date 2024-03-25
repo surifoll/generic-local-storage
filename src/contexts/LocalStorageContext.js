855aad5ad5b81c7df7475d6fdfc3937ccfaa1e93
surifoll <surifoll@yahoo.com>
2024-03-25 13:28:56 +0100

    quick fix for storage sync

--- src/contexts/LocalStorageContext.js
+++ src/contexts/LocalStorageContext.js
@@ -10,10 +10,17 @@ export const LocalStorageProvider = ({ children }) => {
     useEffect(() => {
         const handleStorageChange = (event) => {
             const { key, newValue } = event;
-            setStoredValues((prevValues) => ({
-                ...prevValues,
-                [key]: newValue ? JSON.parse(newValue) : null,
-            }));
+            if (key && newValue) {
+                try {
+                    const parsedValue = JSON.parse(newValue);
+                    setStoredValues((prevValues) => ({
+                        ...prevValues,
+                        [key]: parsedValue,
+                    }));
+                } catch (error) {
+                    console.error('Error parsing JSON:', error);
+                }
+            }
         };
 
         window.addEventListener('storage', handleStorageChange);
@@ -35,6 +42,29 @@ export const LocalStorageProvider = ({ children }) => {
             console.error('Error setting data to localStorage:', error);
         }
     };
+
+
+    const syncStoredValues = () => {
+        for (let i = 0; i < window.localStorage.length; i++) {
+            const key = window.localStorage.key(i);
+            if (key && !storedValues.hasOwnProperty(key)) {
+                try {
+                    const parsedValue = JSON.parse(window.localStorage.getItem(key));
+                    setStoredValues((prevValues) => ({
+                        ...prevValues,
+                        [key]: parsedValue,
+                    }));
+                } catch (error) {
+                    console.error('Error parsing JSON:', error);
+                }
+            }
+        }
+    };
+
+
+    useEffect(() => {
+        syncStoredValues();
+    }, []);
 
     return (
         <LocalStorageContext.Provider value={{ storedValues, setValue }}>
